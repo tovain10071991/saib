baac32b67ae59f5a791e771c8e59c9daea7658ca
skyin <tovain1991@gmail.com>
2015-11-15 11:51:31 +0800

    modify travel with function into travel with execSec like xed in get_next_inst_offset()

--- src/common.cpp
+++ src/common.cpp
@@ -16,7 +16,7 @@ std::map<std::string, int> file_count;
 #endif
 
 static uint32_t current_offset = 0;		//当前返回的指令字节的文件偏移，初始为0，表示第一次要返回第一个指令字节，下次再为0表示遍历结束
-//static int current_function_index = -1;	//当前函数索引，可用来访问函数列表。初始为-1，表示还未开始读
+static int current_execSec_index = -1;	//当前可执行节索引，可用来访问可执行节列表。初始为-1，表示还未开始读
 
 /*===========================================
  * 函数名：	global_init
@@ -38,21 +38,23 @@ void global_init(const string& file_path)
 /*===========================================
  * 函数名：	get_next_inst_offset
  * 参数：
- *	功能描述：返回下一条指令的偏移，反正就一直往下返回指令偏移，所以需要维护一个statis变量表示当前的指令偏移。遍历指令是只遍历函数中的指令，也就是读完一个函数，再读下一个函数，所以还有维护一个static变量表示当前正在读的函数，还要维护一个函数列表(函数列表放在elf_handler中维护吧)
+ *	功能描述：返回下一条指令的偏移，反正就一直往下返回指令偏移，所以需要维护一个statis变量表示当前的指令偏移。遍历指令是只可执行节中的指令，也就是读完一个可执行节，再读下一个可执行节，所以还有维护一个static变量表示当前正在读的可执行节，还要维护一个可执行节列表(可执行节列表放在elf_handler中维护吧)
  *	返回值：void，只准成功
  *	抛出异常
 ===========================================*/
 uint32_t get_next_inst_offset()
 {
+	if(current_execSec_index==-1)	//第一次读
+		goto update_execSec;
 	inst_byte_set_t inst_byte_set = get_byte_set_from_offset(current_offset);
-	current_offset += 15;
-/*	current_offset = current_offset+get_inst_size(inst_byte_set);
+	current_offset = current_offset+get_inst_size(inst_byte_set);
 	if(!is_offset_in_function(current_offset, current_function_index))
 	{
-		++current_function_index;
+update_execSec:
+		++current_execSex_index;
 		current_offset = get_function_offset(current_function_index);
 		inst_byte_set_t inst_byte_set = get_byte_set_from_offset(current_offset);
 		maybe_next_inst_offset = current_offset+get_inst_size(inst_byte_set);
 	}
-*/	return current_offset;
+	return current_offset;
 }
