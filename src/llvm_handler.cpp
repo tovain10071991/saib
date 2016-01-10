550088ae7516c1207fd0de4fa0469559052db9fd
skyin <tovain1991@gmail.com>
2016-01-10 12:36:52 +0800

    iterate inst and judge indirect call

--- src/llvm_handler.cpp
+++ src/llvm_handler.cpp
@@ -36,6 +36,10 @@ using namespace llvm;
 	#define debug_print(x)
 #endif
 
+static void iterate_func_inst(Module* mdl);
+static void iterate_inst(Function* func);
+static bool is_indirect_branch(Instruction* inst);
+static bool is_indirect_call(Instruction* inst);
 
 /*===========================================
  * 函数名：	get_bitcode
@@ -73,45 +77,94 @@ void parse_bitcode(const std::string& bitcode_file)
 	Module* mdl = ParseBitcodeFile(&*buf, context);
 	if(!mdl)
 		errx(-1, "parse bitcode file failed.");
-	debug_output_with_FILE(stderr, "succeed to  create Module\n");
+	debug_output_with_FILE(stderr, "succeed to create Module\n");
 	//迭代函数然后迭代指令
+	debug_output_with_FILE(stderr, "now iterate func\n");
+	iterate_func_inst(mdl);
+}
+
+/*===========================================
+ * 函数名：	iterate_func_inst
+ * 参数：
+ *			Module* mdl
+ *	功能描述：迭代指定模块的函数，然后迭代函数中的指令
+ *	返回值
+ *	抛出异常
+===========================================*/
+static void iterate_func_inst(Module* mdl)
+{
 	for(auto func_iter = mdl->begin(); func_iter != mdl->end(); ++func_iter)
 	{
 		debug_output_with_filePath("out/ins_iter.out", "\nFucntion: %s\n", func_iter->hasName()?func_iter->getName().data():"noname");
-		for(auto inst_iter = inst_begin(&*func_iter); inst_iter != inst_end(&*func_iter); ++inst_iter)
+		//迭代指令
+		debug_output_with_FILE(stderr, "now iterate inst in %s\n", func_iter->hasName()?func_iter->getName().data():"noname");
+		iterate_inst(&*func_iter);
+	}
+}
+
+/*===========================================
+ * 函数名：	iterate_inst
+ * 参数：
+ *			Function* func
+ *	功能描述：迭代指定函数中的指令，判断指令是否为间接分支，是则继续分析
+ *	返回值
+ *	抛出异常
+===========================================*/
+static void iterate_inst(Function* func)
+{
+	for(auto inst_iter = inst_begin(func); inst_iter != inst_end(func); ++inst_iter)
+	{
+		debug_output_with_filePath("out/ins_iter.out", "\n\t%s\t%s\n\t\t%s\n", inst_iter->hasName()?inst_iter->getName().data():"noname", inst_iter->getOpcodeName(), debug_print(inst_iter).c_str());
+		if(is_indirect_branch(&*inst_iter))
 		{
-			debug_output_with_filePath("out/ins_iter.out", "\n\t%s\t%s\n\t\t%s\n", inst_iter->hasName()?inst_iter->getName().data():"noname", inst_iter->getOpcodeName(), debug_print(inst_iter).c_str());
-			if(inst_iter->getOpcode()==Instruction::Call)
+		}
+	}
+}
+
+/*===========================================
+ * 函数名：	is_indirect_branch
+ * 参数：
+ *			Instruction* inst
+ *	功能描述：判断指令是否为间接分支，间接分支包括间接call/jmp/ret
+ *	返回值
+ *	抛出异常
+===========================================*/
+static bool is_indirect_branch(Instruction* inst)
+{
+	if(is_indirect_call(inst))
+		return true;
+	else
+		return false;
+}
+
+static bool is_indirect_call(Instruction* inst)
+{
+	if(inst->getOpcode()==Instruction::Call)
+	{
+		if(CallInst* call_inst = dyn_cast<CallInst>(inst))
+		{
+			if(IntrinsicInst* intrinsic_inst = dyn_cast<IntrinsicInst>(inst))	//固有函数跳过不分析
 			{
-				if(CallInst* call_inst = dyn_cast<CallInst>(&*inst_iter))
-				{
-					if(IntrinsicInst* intrinsic_inst = dyn_cast<IntrinsicInst>(&*inst_iter))
-					{
-						debug_output_with_filePath("out/ins_iter.out", "\tinst_type: intrinsic_inst\n");
-						continue;
-					}
-					if(call_inst->getCalledFunction()!=NULL)
-					{
-						debug_output_with_filePath("out/ins_iter.out", "\tinst_type: direct_call\n");
-						continue;
-					}
-					Value* called_val = call_inst->getCalledValue();
-					debug_output_with_filePath("out/ins_iter.out", "\tcalled_val: %d:%s\t%s\n", called_val->getType()->getTypeID(), debug_print(called_val->getType()).c_str(), debug_print(called_val).c_str());
-					if(User* user = dyn_cast<User>(called_val))
-					{
-						if(user->getNumOperands())
-						{
-							debug_output_with_filePath("out/ins_iter.out", "\tcalled_val's_opr: %u\n", user->getNumOperands());
-							if(Function* func = dyn_cast<Function>(user->getOperand(0)))
-							{
-								debug_output_with_filePath("out/ins_iter.out", "\t\t#####%s\n", debug_print(func).c_str());
-							}
-						}
-					}
-					else
-						errx(-1, "called_val is not user.");
-				}
+				debug_output_with_filePath("out/ins_iter.out", "\tinst_type: intrinsic_inst\n");
+				return false;
+			}
+			else if(call_inst->getCalledFunction()!=NULL)		//只是直接调用
+			{
+				debug_output_with_filePath("out/ins_iter.out", "\tinst_type: direct_call\n");
+				return false;
+			}
+			else
+			{
+				debug_output_with_filePath("out/ins_iter.out", "\tinst_type: indirect_call\n");
+				return true;
 			}
 		}
+		else
+			errx(-1, "really?");	//这里应该不会达到
+	}
+	else
+	{
+		debug_output_with_filePath("out/ins_iter.out", "\tinst_type: not_call\n");
+		return false;
 	}
 }
